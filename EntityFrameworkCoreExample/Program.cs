d7936a46f2add3bbf00ceeec4d8e68bd19b1d40a
Wyatt Smith <wyattjorgen@gmail.com>
2024-07-29 14:06:13 -0700

    Added test code for create and select

--- EntityFrameworkCoreExample/Program.cs
+++ EntityFrameworkCoreExample/Program.cs
@@ -1,6 +1,34 @@
-﻿class Program
+﻿using EntityFrameworkCoreExample;
+
+StudentContext dbContext = new();
+
+// Add with EF Core
+Student s = new()
 {
-	static void Main(string[] args)
-	{
-	}
+	FullName = "Wyatt Smith",
+	EmailAddress = "Wyattjorgen@gmail.com",
+	DateOfBirth = new DateTime(2006, 10, 11)
+};
+
+Student s2 = new()
+{
+	FullName = "Joe Ortiz",
+	EmailAddress = "Joeortiz@gmail.com",
+	DateOfBirth = new DateTime(2000, 1, 1)
+};
+
+dbContext.Add(s); // Prepares the Student insert statement
+dbContext.SaveChanges(); // Executes pending insert. (In Actuality: Execute any pending insert)
+
+dbContext.Add(s2); 
+dbContext.SaveChanges();
+
+// Retrieve Students from DB
+List<Student> allStudents = dbContext.Students.ToList(); // Method syntax
+//allStudents = (from stu in dbContext.Students
+//			   select stu).ToList(); // Querey syntax
+foreach (Student stu in allStudents)
+{
+    Console.WriteLine($"{stu.FullName} has an email of {stu.EmailAddress}");
+    Console.WriteLine();
 }
